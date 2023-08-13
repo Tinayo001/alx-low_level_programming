d5a1b92fddceed23722a22aeaa79515f448c294f
Tinayo001 <keiyatinayo@gmail.com>
2023-08-13 00:43:27 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
 	char a;
 
-	for (a= 0; a < 10; a++)
+	for (a = 0; a < 10; a++)
 		printf("%d", a);
 	putchar('\n');
 	return (0);
