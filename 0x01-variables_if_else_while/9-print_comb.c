b568132c89d22bf7a47d936c907b6a6c84e81213
Tinayo001 <keiyatinayo@gmail.com>
2023-08-13 02:57:03 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+		if (a < 9)
+		   printf(", ");
+	}
+	printf("\n");
+
+	return 0;
+}
+	
