e1f19536d906c881e8063aa9fd337aae95ef5974
root <keiyatinayo@gmail.com>
2023-11-23 01:26:13 -0800

    Write a function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * binary_to_uint - function converts a binary number to unsigned int.
+ * binary_to_uint - function converts a binary number to an unsigned int.
  * @b: pointer to a string containing a binary number
- * Return: unsigned int with decimal value of binary number,or 0 if error
+ *
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num;
 	int a;
+	unsigned int num;
 
 	num = 0;
-
-	if (b == NULL)
+	if (!b)
 		return (0);
-
 	for (a = 0; b[a] != '\0'; a++)
 	{
 		if (b[a] != '0' && b[a] != '1')
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <string.h>
-#include <math.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
