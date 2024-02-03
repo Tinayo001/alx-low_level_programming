ede763c0caaeed5a484ce297315839aca41947cc
Tinayo001 <keiyatinayo@gmail.com>
2024-02-03 00:27:43 -0800

    strstr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,13 +12,13 @@ unsigned int _strspn(char *s, char *accept)
 	int i;
 	int j;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
+	for(i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[i] != accept[j] ; j++)
+		for(j = 0; s[i] != accept[j]; j++)
 		{
-			if (accept[j] == '\0')
-				return (i);
+			if(accept[j] != '\0')
+				return i;
 		}
 	}
-	return (0);
+	return 0;
 }
