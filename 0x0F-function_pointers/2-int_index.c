7eebfff2059aeaa1b38d27ad0c7e15bcd92e3227
Tinayo001 <keiyatinayo@gmail.com>
2024-02-05 00:38:32 -0800

    function that searches for an integer in an array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,15 +13,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if(array != NULL && cmp != NULL && size > 0)
 	{
-		if (cmp(array[i]) == 1)
-			return (i);
+		for(i = 0; i < size; i++)
+		{
+			if(cmp(array[i]) != 0)
+			{
+				return i;
+			}
+		
+		}
 	}
-
-	return (-1);
+	return -1;
 }
