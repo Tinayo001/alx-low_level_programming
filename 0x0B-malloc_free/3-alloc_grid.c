632a7b24577478bb9e4dde27ca2db573ae721855
Tinayo001 <keiyatinayo@gmail.com>
2024-02-06 05:09:57 -0800

    function that allocates memory to a pointer pointing to two arrays

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,26 +10,32 @@
 int **alloc_grid(int width, int height)
 {
 	int **output;
-	int i, j;
+	int i,j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	output = malloc(sizeof(int *) * height);
-	if (output == NULL)
-		return (NULL);
-	for (i = 0; i < height; i++)
-	{
-		output[i] = malloc(sizeof(int) * width);
-
-		if (output[i] == NULL)
+	if(output == NULL)
 		{
-			free(output);
-			for (j = 0; j <= height; j++)
-				free(output[j]);
 			return (NULL);
 		}
+	for(i = 0; i < height; i++)
+	{
+		output[i] = malloc(sizeof(int) * width);
+		if(output[i] == NULL)
+		{
+			for (j = 0; j < i; i++)
+			{
+				free(output[j]);
+				free(output);
+				return (NULL);
+			}
+		}
 		for (j = 0; j < width; j++)
+		{
 			output[i][j] = 0;
+		}
+		
 	}
 	return (output);
 }
