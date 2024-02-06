9a619970960bef1fdad123543deb111b0f134f28
Tinayo001 <keiyatinayo@gmail.com>
2024-02-06 02:09:47 -0800

    allocating memory to a pointer pointing to another location

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,19 +9,28 @@
 char *_strdup(char *str)
 {
 	int i;
-	char *copy;
 	int count = 0;
+	char *copy;
 
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; str[i] != '\0'; i++)
+	{
 		count++;
-	copy = malloc(sizeof(char) * count + 1);
-
-	if (copy == NULL)
-		return (NULL);
+	}
+	copy = malloc(sizeof(char) * ( count + 1));
+	{
+		if (copy == NULL)
+		{
+			return (NULL);
+		}
+	}
 	for (i = 0; str[i] != '\0'; i++)
+	{
 		copy[i] = str[i];
-
+	}
+	copy[count] = '\0';
 	return (copy);
 }
