0b1f1b186958a7845caee27b29e820a4e376117b
Tinayo001 <keiyatinayo@gmail.com>
2024-02-06 00:55:49 -0800

    allocation of memory to arrays

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,15 +10,22 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *arr;
+	char *array;
 
-	if (size == 0)
-		return (NULL);
-
-	arr = malloc(sizeof(char) * size);
-	if (arr == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		arr[i] = c;
-	return (arr);
+	if(size == 0)
+	{
+		return NULL;
+	}
+	array = malloc(sizeof(char) * size);
+	{
+		if(array == 0)
+		{
+			return NULL;
+		}
+		for(i = 0; i < size; i++)
+		{
+			array[i] = c;
+		}
+	}
+	return array;
 }
