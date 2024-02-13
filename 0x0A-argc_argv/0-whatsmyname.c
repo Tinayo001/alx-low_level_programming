a740c6449e5f2549a525cc95b49534d23305345e
Tinayo001 <keiyatinayo@gmail.com>
2024-02-12 23:31:53 -0800

    argument to print name of a program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,13 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argc;
-
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+	{
+		printf ("%s\n", argv[0]);
+	}
+	else
+	{
+		printf ("Program not found\n");
+	}
 	return (0);
 }
