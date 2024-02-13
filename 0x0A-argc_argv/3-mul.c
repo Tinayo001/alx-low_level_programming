d048de5208361b52ec40920712d48b9163938d24
Tinayo001 <keiyatinayo@gmail.com>
2024-02-13 00:11:53 -0800

    function that prints the multiplication of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,11 +10,18 @@
 
 int main(int argc, char *argv[])
 {
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result;
+
 	if (argc != 3)
 	{
-		printf("Error\n");
+		printf ("error");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	result = num1 * num2;
+
+	printf ("%d\n", result);
+
 	return (0);
 }
